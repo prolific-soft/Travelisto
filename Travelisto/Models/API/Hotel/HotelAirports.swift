09f2010387b9bcbaa4de33ed7b8778ae44059268
Chidi Emeh <chidiemeh184@gmail.com>
2017-09-28 11:36:31 -0400

    End Hotel Airports

--- Travelisto/Models/API/Hotel/HotelAirports.swift
+++ Travelisto/Models/API/Hotel/HotelAirports.swift
@@ -47,30 +47,95 @@ struct HotelAirports : Codable {
         }
         
         let amenities : [Amenity]
-        let awards : [Any]
-        let images : [Any]
-        let rooms : [Any]
-        let links : Any
-    }
-    
-    private enum CodingKeys: String, CodingKey {
+        let awards : [String]?   //Check for this two
+        let images : [URL]?    //Check for this value
         
-        case propertyCode = "property_code"
-        case propertyName = "property_name"
-        case marketingText = "marketing_text"
-        case location
-        case address
-        case totalPrices = "total_price"
-        case minDailyRate
-        case contacts
-        case amenities
-        case awards
-        case images
-        case rooms
-        case links
-    }
+        
+        struct Room : Codable {
+            let bookingCode : String
+            let roomTypeCode : String
+            let ratePlanCode : String
+            let totalAmount : Amount
+            
+            struct Rate : Codable {
+                let startDate : Date
+                let endDate : Date
+                let currencyCode : String
+                let price : Double
+                
+                private enum CodingKeys : String, CodingKey {
+                    case startDate = "start_date"
+                    case endDate  = "end_date"
+                    case currencyCode = "currency_code"
+                    case price
+                }
+            }
+            
+            let rates : [Rate]
+            let descriptions : [String]
+            
+            struct RoomType : Codable {
+                let roomType : String
+                
+                private enum CodingKeys : String, CodingKey {
+                    case roomType = "room_type"
+                }
+            }
+            
+            let roomTypeInfo : RoomType
+            let rateTypeCode : String
+            
+            private enum CodingKeys : String, CodingKey {
+                case bookingCode = "booking_code"
+                case roomTypeCode = "room_type_code"
+                case ratePlanCode = "rate_plan_code"
+                case totalAmount = "total_amount"
+                case rates
+                case descriptions
+                case roomTypeInfo  = "room_type_info"
+                case rateTypeCode = "rate_type_code"
+            }
+        }
+        
+        let rooms : [Room]
+        
+        struct Link : Codable {
+            
+            struct Href : Codable {
+                let href : URL
+            }
+            
+            let moreRoomsAtThisHotel : Href
+            private enum CodingKeys : String, CodingKey {
+               case moreRoomsAtThisHotel = "more_rooms_at_this_hotel"
+            }
+        }
+        
+        let links : Link
+        
+        private enum CodingKeys: String, CodingKey {
+            
+            case propertyCode = "property_code"
+            case propertyName = "property_name"
+            case marketingText = "marketing_text"
+            case location
+            case address
+            case totalPrices = "total_price"
+            case minDailyRate
+            case contacts
+            case amenities
+            case awards
+            case images
+            case rooms
+            case links
+        }
+        
+        
+    }//End Hotel
     
-    //let results : [Any]
+    
+    
+    let results : [Hotel]
 }
 
 
