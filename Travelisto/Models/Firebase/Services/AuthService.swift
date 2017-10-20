451976f5d08f44d9cb4083468fdde9b0451320f4
Chidi Emeh <chidiemeh184@gmail.com>
2017-10-20 13:19:41 -0400

    Added Firebase Libraries

---
+++ Travelisto/Models/Firebase/Services/AuthService.swift
@@ -0,0 +1,25 @@
+//
+//  AuthService.swift
+//  Travelisto
+//
+//  Created by Chidi Emeh on 10/20/17.
+//  Copyright © 2017 Chidi Emeh. All rights reserved.
+//
+
+import Foundation
+import FirebaseAuth
+import FirebaseStorage
+import FirebaseDatabase
+
+
+
+/// Handles Authentication services for Users
+/// in Firebase
+struct AuthService {
+    
+    
+    
+    
+    
+}// End Struct AuthService
+
