15674ca33ff4e54b3d097b5dcf384571b2ee676a
Chidi Emeh <chidiemeh184@gmail.com>
2018-04-11 22:08:46 -0400

    hotel Deals

--- Travelisto/SEARCH/HOTEL SUGGESTION/hoteL Deals/HotelDealsTVCell.swift
+++ Travelisto/SEARCH/HOTEL SUGGESTION/hoteL Deals/HotelDealsTVCell.swift
@@ -8,17 +8,65 @@
 
 import UIKit
 
-class HotelDealsTVCell: UITableViewCell {
+class HotelDealsTVCell: UITableViewCell, UICollectionViewDelegate, UICollectionViewDataSource,
+UICollectionViewDelegateFlowLayout {
 
+    
+       @IBOutlet weak var HotelDealsCollectionView: UICollectionView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        HotelDealsCollectionView.delegate = self
+        HotelDealsCollectionView.dataSource = self
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
 
+}
+
+
+
+//MARK: - Layouts
+extension HotelDealsTVCell {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let cellDefaultHeight = CGFloat(119)
+        let cellDefaultWidth = CGFloat(323)
+        let size = CGSize(width: cellDefaultWidth, height: cellDefaultHeight)
+        return size
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        let inset = UIEdgeInsetsMake(10, 16, 0, 0)
+        return inset
+    }
+    
+}
+
+
+
+//MARK: - Datasource
+extension HotelDealsTVCell {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FlightSuggestionCell.hotelDealsCVCell.rawValue, for: indexPath) as! HotelDealsCVCell
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        //Perform Segue
+    }
+    
+    
 }
