2e4ef6d8880f459b877c927aca32be97b1c23568
Chidi Emeh <chidiemeh184@gmail.com>
2018-04-13 23:39:57 -0400

    Autoresizing Privacy

--- Travelisto/SETTINGS/PRIVACY/PrivacyTVC.swift
+++ Travelisto/SETTINGS/PRIVACY/PrivacyTVC.swift
@@ -29,7 +29,7 @@ class PrivacyTVC: UITableViewController {
 extension PrivacyTVC {
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 600
+        return UITableViewAutomaticDimension
     }
     
 }
