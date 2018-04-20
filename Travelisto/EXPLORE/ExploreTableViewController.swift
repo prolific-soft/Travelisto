8bc3add97f67c3764cc18d673bcc052dfca5f492
Chidi Emeh <chidiemeh184@gmail.com>
2018-04-19 22:19:33 -0400

    Refactored to MVC in Explore : TVC owns both collectionviews

--- Travelisto/EXPLORE/ExploreTableViewController.swift
+++ Travelisto/EXPLORE/ExploreTableViewController.swift
@@ -20,6 +20,9 @@ class ExploreTableViewController : UITableViewController {
         return refreshControl
     }()
     
+    //TableView cell
+    var tripToCell : TripToTVCell!
+    var relaxationCell : RelaxationTVCell!
     
     
     override func viewDidLoad() {
@@ -102,3 +105,123 @@ extension ExploreTableViewController {
     }
     
 }
+
+
+//MARK: WILL DISPLAY
+extension ExploreTableViewController {
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        
+        if indexPath.row == 0 {
+            guard let cell = cell as? TripToTVCell else { return }
+            let tripToCollectionView = cell.tripToCollectionView
+            tripToCollectionView?.delegate = self
+            tripToCollectionView?.dataSource = self
+        }else {
+            guard let cell = cell as? RelaxationTVCell else { return }
+            let relaxationCollectionView = cell.relaxationCollectionView
+            relaxationCollectionView?.delegate = self
+            relaxationCollectionView?.dataSource = self
+        }
+    }
+    
+}
+
+
+
+//MARK: - CollectionView Delegate & Datasource
+extension ExploreTableViewController : UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        
+        //CollectionView Tag 1 is tripToCollectionView
+        //CollectionView Tag 1 is tripToCollectionView
+        if collectionView.tag == 1 {
+            return 30
+        }else {
+            return 15
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        if collectionView.tag == 1 {
+                let cellDefaultHeight = CGFloat(200)
+                let cellDefaultWidth = CGFloat(335)
+                let size = CGSize(width: cellDefaultWidth, height: cellDefaultHeight)
+                return size
+        }else {
+            let cellDefaultHeight = CGFloat(168)
+            let cellDefaultWidth = CGFloat(135)
+            let size = CGSize(width: cellDefaultWidth, height: cellDefaultHeight)
+            return size
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        
+        if collectionView.tag == 1 {
+            let inset = UIEdgeInsetsMake(20, 20, 20, 0)
+            return inset
+        }else {
+            let inset = UIEdgeInsetsMake(2, 20, 2, 0)
+            return inset
+        }
+    }
+    
+}
+
+
+
+//MARK: - Datasource
+extension ExploreTableViewController {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return collectionView.tag == 1 ? 3 : 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        if collectionView.tag == 1 {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ExploreCells.tripToCVCell.rawValue, for: indexPath) as! TripToCVCell
+            return cell
+        }else {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ExploreCells.relaxationCVCell.rawValue, for: indexPath) as! RelaxationCVCell
+            return cell
+        }
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        //Perform Segue
+    }
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Travelisto/EXPLORE/RELAXATION/RelaxationTVCell.swift
+++ Travelisto/EXPLORE/RELAXATION/RelaxationTVCell.swift
@@ -8,16 +8,15 @@
 
 import UIKit
 
-class RelaxationTVCell: UITableViewCell, UICollectionViewDelegate, UICollectionViewDataSource,
-UICollectionViewDelegateFlowLayout  {
+class RelaxationTVCell: UITableViewCell {
 
     @IBOutlet weak var relaxationCollectionView: UICollectionView!
     
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        relaxationCollectionView.delegate = self
-        relaxationCollectionView.dataSource = self
+//        relaxationCollectionView.delegate = self
+//        relaxationCollectionView.dataSource = self
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -27,47 +26,48 @@ UICollectionViewDelegateFlowLayout  {
 
 }
 
-
-
-//MARK: - Layouts
-extension RelaxationTVCell {
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 15
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let cellDefaultHeight = CGFloat(168)
-        let cellDefaultWidth = CGFloat(135)
-        let size = CGSize(width: cellDefaultWidth, height: cellDefaultHeight)
-        return size
-        
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        let inset = UIEdgeInsetsMake(2, 20, 2, 0)
-        return inset
-    }
-    
-}
-
-
-
-//MARK: - Datasource
-extension RelaxationTVCell {
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ExploreCells.relaxationCVCell.rawValue, for: indexPath) as! RelaxationCVCell
-        return cell
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        //Perform Segue
-    }
-    
-    
-}
+//
+//
+////MARK: - Layouts
+//, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout
+//extension RelaxationTVCell {
+//
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+//        return 15
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        let cellDefaultHeight = CGFloat(168)
+//        let cellDefaultWidth = CGFloat(135)
+//        let size = CGSize(width: cellDefaultWidth, height: cellDefaultHeight)
+//        return size
+//
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+//        let inset = UIEdgeInsetsMake(2, 20, 2, 0)
+//        return inset
+//    }
+//
+//}
+//
+//
+//
+////MARK: - Datasource
+//extension RelaxationTVCell {
+//
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return 10
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ExploreCells.relaxationCVCell.rawValue, for: indexPath) as! RelaxationCVCell
+//        return cell
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+//        //Perform Segue
+//    }
+//
+//
+//}
